1d35148eb828412a77be02300fe3a90b97a17ae3
MASTER-PNZ <paulkeesling@gmail.com>
2018-09-28 14:55:26 -0400

    deleted username loop
    changed the capitalization of the modal text

--- js/main.js
+++ js/main.js
@@ -3,19 +3,18 @@
 const $container = $('.container');
 const $search = $('#search_bar');
 
-// class constructor for random employee info and function to create box div.
+// Class constructor for random employee info and function to create box div.
 
 class RandomUser {
-    constructor (name, location, email, picture, login, cell, dob) {
+    constructor (name, location, email, picture, cell, dob) {
         this.name = name;
         this.location = location;
         this.email = email;
         this.picture = picture;
-        this.login = login;
         this.cell = cell;
         this.dob = dob;
     }
-        // create box div function
+        // Create box div function
        createBox () {
           let boxdiv = `
             <div class="box">
@@ -29,9 +28,7 @@ class RandomUser {
             return boxdiv;
         }
 
-
-
-        // create modal div function including buttons
+  // create modal div function including buttons
         createModal (currentEmployee) {
           let date = this.dob.date.substr(0,10);
           let dateArr = date.split('-');
@@ -45,10 +42,10 @@ class RandomUser {
           <img class="modimg" src="${this.picture.large}" alt/>
           <div class="modemployee_data">
             <h1>${this.name.first} ${this.name.last}</h1>
-            <p class="username">${this.login.username}</p>
             <p class="email">${this.email}</p>
-            <p class="cell-num">${this.cell}</p>
+            <p class="city">${this.location.city}</p>
             <hr>
+            <p class="cell-num">${this.cell}</p>
             <p class="address">${this.location.street}<br/>${this.location.city}, ${this.location.state} ${this.location.postcode}</p>
             <p class="dob">Birthday:${modalDate}</p>
           </div>
@@ -72,13 +69,13 @@ return fetch('https://randomuser.me/api/?results=12&inc=name,location,email,pict
 }
 
 
-// function to push random user data to the employees array and display user divs.
+// Function to push random user data to the employees array and display user divs.
 function userDivs () {
 getUserInfo()
   .then(results => {
     const users = results;
     users.map(usr => {
-      const employee = new RandomUser(usr.name, usr.location, usr.email, usr.picture, usr.login, usr.cell, usr.dob);
+      const employee = new RandomUser(usr.name, usr.location, usr.email, usr.picture, usr.cell, usr.dob);
       employeeArr.push(employee);
     });
       employeeArr.map( employee => {
@@ -94,14 +91,14 @@ getUserInfo()
 
       });
 
-  // event listener for boxes to show current modal window
+  // Event listener for boxes to show current modal window
       $('.box').on('click', function(){
         $(this).next().show();
         $('body').addClass('overlay');
         $('.box').addClass('disabled');
       });
 
-  // event listeners for modal buttons (close, previous, and next).
+  // Event listeners for modal buttons (close, previous, and next).
       $('.close').on('click', function (){
         $(this).parent().hide();
         $('body').removeClass('overlay');
@@ -124,27 +121,18 @@ getUserInfo()
         modDiv.next().next().show();
       });
 
-  // event listener for search_bar
+  // Event listener for search_bar
     $search.on('keyup', function(){
       let userSearch = $search.val().toLowerCase();
-      let name = $('.name');
-      let username = $('.username');
-      for(i = 0; i < name.length; i += 1) {
-        if ( name[i].innerHTML.indexOf(userSearch) > -1 ) {
-          name[i].parentElement.parentElement.style.display = "";
+      let names = $('.name');
+      for(i = 0; i < names.length; i += 1) {
+        if ( names[i].innerHTML.indexOf(userSearch) > -1 ) {
+          names[i].parentElement.parentElement.style.display = "";
         } else {
-          name[i].parentElement.parentElement.style.display = 'none';
-        }
-      }
-      for(i = 0; i < username.length; i += 1) {
-        if ( username[i].innerHTML.indexOf(userSearch) > -1 ) {
-          username[i].parentElement.parentElement.previousElementSibling.style.display = "";
-        } else {
-          username[i].parentElement.parentElement.style.display = 'none';
+          names[i].parentElement.parentElement.style.display = 'none';
         }
       }
     });
-
   });
 }
 
